21d7be7531c5e118f6e889ca6e012752763f1a17
Max Lopez <max@abletp.net>
2025-07-16 13:32:53 -0400

    removed microsoft products are they are to be installed via intune

--- app_deployment.ps1
+++ app_deployment.ps1
@@ -30,35 +30,35 @@ $zoomMsi = "$workDir\Zoom.msi"
 Download-File $zoomUrl $zoomMsi
 Start-Process msiexec.exe -ArgumentList "/i `"$zoomMsi`" /quiet /norestart" -Wait
 
-# 4. Install Teams (per-machine)
-$teamsUrl = "https://statics.teams.cdn.office.net/production-windows-x64/1.00.6774.0/Teams_windows_x64.msi"
-$teamsMsi = "$workDir\Teams.msi"
-Download-File $teamsUrl $teamsMsi
-Start-Process msiexec.exe -ArgumentList "/i `"$teamsMsi`" ALLUSERS=1 /quiet /norestart" -Wait
+# # 4. Install Teams (per-machine)
+# $teamsUrl = "https://statics.teams.cdn.office.net/production-windows-x64/1.00.6774.0/Teams_windows_x64.msi"
+# $teamsMsi = "$workDir\Teams.msi"
+# Download-File $teamsUrl $teamsMsi
+# Start-Process msiexec.exe -ArgumentList "/i `"$teamsMsi`" ALLUSERS=1 /quiet /norestart" -Wait
 
-# 5. Install Office 365 Business (Microsoft 365 Apps for Business)
-# Download Office Deployment Tool
-$odtUrl = "https://download.microsoft.com/download/6/D/4/6D48F0C5-401C-43B2-B3C6-BB46B9CE5A4C/OfficeDeploymentTool.exe"
-$odtExe = "$workDir\ODT.exe"
-Download-File $odtUrl $odtExe
-Start-Process $odtExe -ArgumentList "/quiet /extract:$workDir" -Wait
+# # 5. Install Office 365 Business (Microsoft 365 Apps for Business)
+# # Download Office Deployment Tool
+# $odtUrl = "https://download.microsoft.com/download/6/D/4/6D48F0C5-401C-43B2-B3C6-BB46B9CE5A4C/OfficeDeploymentTool.exe"
+# $odtExe = "$workDir\ODT.exe"
+# Download-File $odtUrl $odtExe
+# Start-Process $odtExe -ArgumentList "/quiet /extract:$workDir" -Wait
 
-# Create Office configuration XML
-$officeXml = @"
-<Configuration>
-  <Add OfficeClientEdition="64" Channel="MonthlyEnterprise">
-    <Product ID="O365BusinessRetail">
-      <Language ID="en-us" />
-    </Product>
-  </Add>
-  <Display Level="None" AcceptEULA="TRUE" />
-  <Property Name="AUTOACTIVATE" Value="1" />
-</Configuration>
-"@
-$officeConfig = "$workDir\OfficeConfig.xml"
-$officeXml | Out-File -Encoding UTF8 -FilePath $officeConfig
+# # Create Office configuration XML
+# $officeXml = @"
+# <Configuration>
+#   <Add OfficeClientEdition="64" Channel="MonthlyEnterprise">
+#     <Product ID="O365BusinessRetail">
+#       <Language ID="en-us" />
+#     </Product>
+#   </Add>
+#   <Display Level="None" AcceptEULA="TRUE" />
+#   <Property Name="AUTOACTIVATE" Value="1" />
+# </Configuration>
+# "@
+# $officeConfig = "$workDir\OfficeConfig.xml"
+# $officeXml | Out-File -Encoding UTF8 -FilePath $officeConfig
 
-# Run Office install
-Start-Process "$workDir\setup.exe" -ArgumentList "/configure $officeConfig" -Wait
+# # Run Office install
+# Start-Process "$workDir\setup.exe" -ArgumentList "/configure $officeConfig" -Wait
 
 Write-Host "✅ All applications installed. Reboot may be required."
