dae090d40b75e865121cbd4c382400b0e4b48147
Max Lopez <max@abletp.net>
2025-07-17 14:35:10 -0400

    Changed the priority order in bloatware_cleanup

--- bloatware_cleanup.ps1
+++ bloatware_cleanup.ps1
@@ -1,6 +1,18 @@
 # Ensure execution policy allows running scripts
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass -Force
 
+# Download and run VBS script silently using CSCRIPT
+$githubVbsUrl = "https://raw.githubusercontent.com/demo7up/abletp/main/remove_c2r.vbs"
+$tempVbsPath = "$env:TEMP\remove_c2r.vbs"
+
+Invoke-WebRequest -Uri $githubVbsUrl -OutFile $tempVbsPath -UseBasicParsing
+
+$output = & cscript.exe //Nologo "$tempVbsPath"
+$output | Out-File "$env:ProgramData\Debloat\vbs_output.log" -Encoding UTF8 -Force
+Write-Output "VBS output saved to: $env:ProgramData\Debloat\vbs_output.log"
+
+Remove-Item $tempVbsPath -Force
+
 # Download bloatware.ps1 and run from GitHub
 $githubPs1Url = "https://raw.githubusercontent.com/demo7up/abletp/main/bloatware.ps1"
 $tempPs1Path = "$env:TEMP\bloatware.ps1"
@@ -21,15 +33,4 @@ Start-Process powershell.exe -ArgumentList "-NoProfile", "-ExecutionPolicy Bypas
 
 Remove-Item $tempPs1Path -Force
 
-# Download and run VBS script silently using CSCRIPT
-$githubVbsUrl = "https://raw.githubusercontent.com/demo7up/abletp/main/remove_c2r.vbs"
-$tempVbsPath = "$env:TEMP\remove_c2r.vbs"
-
-Invoke-WebRequest -Uri $githubVbsUrl -OutFile $tempVbsPath -UseBasicParsing
-
-$output = & cscript.exe //Nologo "$tempVbsPath"
-$output | Out-File "$env:ProgramData\Debloat\vbs_output.log" -Encoding UTF8 -Force
-Write-Output "VBS output saved to: $env:ProgramData\Debloat\vbs_output.log"
-
-Remove-Item $tempVbsPath -Force
 
