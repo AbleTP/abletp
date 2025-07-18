96e63f95b3a6e5c049a917af95a14e009ec890ce
Max Lopez <abletpcoderepo@gmail.com>
2025-07-18 08:51:35 -0400

    removed a txt file

--- bloatware_cleanup.ps1
+++ bloatware_cleanup.ps1
@@ -2,7 +2,7 @@
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass -Force
 
 # Download and run VBS script silently using CSCRIPT
-$githubVbsUrl = "https://raw.githubusercontent.com/demo7up/abletp/main/remove_c2r.vbs"
+$githubVbsUrl = "https://raw.githubusercontent.com/AbleTP/abletp/main/remove_c2r.vbs"
 $tempVbsPath = "$env:TEMP\remove_c2r.vbs"
 
 Invoke-WebRequest -Uri $githubVbsUrl -OutFile $tempVbsPath -UseBasicParsing
@@ -14,7 +14,7 @@ Write-Output "VBS output saved to: $env:ProgramData\Debloat\vbs_output.log"
 Remove-Item $tempVbsPath -Force
 
 # Download bloatware.ps1 and run from GitHub
-$githubPs1Url = "https://raw.githubusercontent.com/demo7up/abletp/main/bloatware.ps1"
+$githubPs1Url = "https://raw.githubusercontent.com/AbleTP/abletp/main/bloatware.ps1"
 $tempPs1Path = "$env:TEMP\bloatware.ps1"
 
 Invoke-WebRequest -Uri $githubPs1Url -OutFile $tempPs1Path -UseBasicParsing
@@ -24,7 +24,7 @@ Start-Process powershell.exe -ArgumentList "-NoProfile", "-ExecutionPolicy Bypas
 Remove-Item $tempPs1Path -Force
 
 # Download device_rename.ps1 and run from GitHub
-$githubPs1Url = "https://raw.githubusercontent.com/demo7up/abletp/main/app_deployment.ps1"
+$githubPs1Url = "https://raw.githubusercontent.com/AbleTP/abletp/main/app_deployment.ps1"
 $tempPs1Path = "$env:TEMP\app_deployment.ps1"
 
 Invoke-WebRequest -Uri $githubPs1Url -OutFile $tempPs1Path -UseBasicParsing
